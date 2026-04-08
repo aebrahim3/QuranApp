b108284d0d889b018a542b26b404f512e0a12398
amjadh ebrahim <aebrahim3@my.bcit.ca>
2026-04-08 12:19:42 -0700

    deleted some dto classes and replaced them @SerializedName in one class

--- app/src/main/java/com/bcit/quranapp/dataModel/Surah.kt
+++ app/src/main/java/com/bcit/quranapp/dataModel/Surah.kt
@@ -1,12 +1,23 @@
 package com.bcit.quranapp.dataModel
 
+import com.bcit.quranapp.dto.TranslatedNameDto
+import com.google.gson.annotations.SerializedName
+
 data class Surah(
+    @SerializedName("id")
     val number: Int,
+    @SerializedName("name_arabic")
     val arabicName: String,
+    @SerializedName("name_complex")
     val englishName: String,
-    val englishNameTranslation: String,
+    @SerializedName("translated_name")
+    val englishNameTranslation: TranslatedNameDto,
+    @SerializedName("verses_count")
     val numberOfAyahs: Int,
+    @SerializedName("revelation_place")
     val revelationPlace: String,
+    @SerializedName("revelation_order")
     val revelationOrder: Int,
+    @SerializedName("bismillah_pre")
     val bismillahPre: Boolean
 )
--- app/src/main/java/com/bcit/quranapp/dto/ChaptersResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.bcit.quranapp.dto
-
-/**
- * Refers to the "chapters" heading in the
- * JSON file. It contains a list of chapters.
- */
-data class ChaptersResponse(
-    val chapters: List<ChapterDto>
-)
