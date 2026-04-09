1efced8b84cb9df1f1e527ea71a053fcee9daee4
amjadh ebrahim <aebrahim3@my.bcit.ca>
2026-04-09 09:18:12 -0700

    Trying to document an understanding of what's going on in the backend.

--- app/src/main/java/com/bcit/quranapp/di/AppModule.kt
+++ app/src/main/java/com/bcit/quranapp/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.bcit.quranapp.di
 
+import com.bcit.quranapp.network.QuranApi
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
@@ -17,11 +18,28 @@ object AppModule {
     @Singleton
     fun provideFirebaseAuth(): FirebaseAuth = FirebaseAuth.getInstance()
 
+    /**
+     * This instance sends a GET request to the API.
+     * It builds a QuranApi instance based on the interface and
+     * pre-programs it to tell Retrofit to send a GET request to the API and
+     * return a ChaptersResponse object.
+     * It hands over the JSON data to GSON,
+     * which converts it to a ChaptersResponse object.
+     */
     @Provides
     @Singleton
     fun provideRetrofit(): Retrofit = Retrofit.Builder()
         .baseUrl("https://api.quran.com/api/v4/")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
+
+    /**
+     * This instance makes it easier to getSurahs() because we just
+     * have to tack on it getSurahs(). It tells Retrofit to send a GET
+     * request to the API and return a ChaptersResponse object.
+     */
+    @Provides
+    @Singleton
+    fun provideQuranApi(retrofit: Retrofit) = retrofit.create(QuranApi::class.java)
 }
 
--- app/src/main/java/com/bcit/quranapp/network/QuranApi.kt
+++ app/src/main/java/com/bcit/quranapp/network/QuranApi.kt
@@ -1,5 +1,10 @@
 package com.bcit.quranapp.network
 
+import com.bcit.quranapp.dataModel.ChaptersResponse
+import retrofit2.http.GET
+
 interface QuranApi {
+    @GET("chapters")
+    suspend fun getSurahs(): ChaptersResponse
 
 }
